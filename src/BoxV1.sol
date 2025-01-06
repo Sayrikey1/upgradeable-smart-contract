9f8d0a6d5265050e7878b90cdf57213a4877b7e6
Sayrikey <kamalseriki49@gmail.com>
2025-01-06 02:02:31 +0100

    upgrades work

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,18 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
+pragma solidity ^0.8.19;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-contract BoxV1 {
+
+// storage is stored in the proxy contract, NOT in the implementation contract
+
+// proxy -> deploy implementation contract -> call some "initializer" function -> set implementation contract address in proxy contract -> call implementation contract functions
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable{
     uint256 internal number;
 
-    function getNumber() external view returns (uint256) {
+    // We can't use constructors with our proxies, 
+    // because constructors add storage to the contract,
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }    
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender); // sets owner to msg.sender
+        __UUPSUpgradeable_init(); // best practice to call this after Ownable
+    }
+
+    function getValue() external view returns (uint256) {
         return number;
     }
 
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {
+
+    function setValue(uint256 _number) external {
         number = _number;
     }
 
-    function getNumber() external view returns (uint256) {
+    function getValue() external view returns (uint256) {
         return number;
     }
 
     function version() external pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
